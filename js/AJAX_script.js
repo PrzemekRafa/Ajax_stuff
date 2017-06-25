7680553b281c28872736858f70e87cc20c588c62
Przemek Rafa Mac <przemyslaw.rafa@gmail.com>
2017-06-25 13:50:25 +0200

    AJAX z obrobka danych gotowy

--- js/AJAX_script.js
+++ js/AJAX_script.js
@@ -26,14 +26,24 @@ function ajax (ajaxoOptions) {
 //  OTWARCIE POLACZENIA
     httpReq.open(options.type, options.url, true);
     
-//  SPRAWDZANIE STANU POLACZENIA
+//  SPRAWDZANIE STANU POLACZENIA FUNCKJA httpSuccess
     httpReq.onreadystatechange = function() {
         if (this.readyState == 4) {
             if (httpSuccess (this)) {
                 console.log ('Połączenie działa');
                 console.log (this.readyState);
                 console.log (this.status);
+                
+//              JESLI DANE W FORMACIE XML TO ZWROCI OBIEKT responseXML - W PRZECIWNYM WYPADKU responseText (JSON)
+                var returnData = (options.dataType=='xml')? this.responseXML: this.responseText;
+                
+                console.log(returnData);
+                
+                options.onSuccess(returnData);
+                
                 httpReq = null;
+            } else {
+                options.onError(console.log('BŁĄD!'));
             }
         }
     }
@@ -46,5 +56,22 @@ function ajax (ajaxoOptions) {
 ajax({
     type: 'GET',
     url: 'http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl',
+    onError: function (msg) {
+        console.log(msg);
+    },
+    onSuccess: function (reponse) {
+        var jsonObj = JSON.parse(reponse);
+        console.log(jsonObj.userId);
+        console.log(jsonObj.userName);
+        console.log(jsonObj.userURL);
+        
+        var userId = jsonObj.userId;
+//      jQuery
+//        $('#testowy').text(userId);
+//      TO SAMO CO WYŻEJ CZYSTYM JS
+        document.getElementById('testowy').innerHTML = userId;
+        
+//        document.write(jsonObj.userName);
+    }
 });
 
